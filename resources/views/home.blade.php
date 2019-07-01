0f8e854b0e2faa3cc928c61d7b1077a64105e956
vioputriy <vioputriyuwan@gmail.com>
2019-07-01 21:43:05 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/63d78ad6c0866bfa07c0960642745ad2/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" class="rounfed-circle">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-9 pt=5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+            <div class="pr-5"><strong>153</strong>posts</div>
+            <div class="pr-5"><strong>23k</strong>followers</div>
+            <div class="pr-5"><strong>212</strong>following</div>
+            </div>
+            <div class="pt-4" font-wight-bold>freeCodeCamp.org</div>
+        <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+        <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row">
+    <div class="col-4">
+     <img src="<ul class="Ln-UN"><li class="-V_eO"><span>758</span><span class="_1P1TY coreSpriteHeartSmall"></span></li><li class="-V_eO"><span>17</span><span class="_1P1TY coreSpriteSpeechBubbleSmall"></span></li></ul>">
+    </div>
+</div>
+@endsection
