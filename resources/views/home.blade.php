0bee70332100a194f574c7ff4bcf57d15e419db5
vioputriy <vioputriyuwan@gmail.com>
2019-07-02 16:37:19 +0700

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,29 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-        <img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/63d78ad6c0866bfa07c0960642745ad2/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" class="rounfed-circle">
+        <img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/63d78ad6c0866bfa07c0960642745ad2/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" class="rounded-circle">
         </div>
-    </div>
-    <div class="row">
         <div class="col-9 pt=5">
-            <div><h1>freecodecamp</h1></div>
+            <div><h1></h1></div>
             <div class="d-flex">
-            <div class="pr-5"><strong>153</strong>posts</div>
-            <div class="pr-5"><strong>23k</strong>followers</div>
-            <div class="pr-5"><strong>212</strong>following</div>
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>23k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
             </div>
             <div class="pt-4" font-wight-bold>freeCodeCamp.org</div>
         <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
         <div><a href="#">www.freecodecamp.org</a></div>
         </div>
     </div>
-    <div class="row">
+    <div class="row pt-4">
     <div class="col-4">
-     <img src="<ul class="Ln-UN"><li class="-V_eO"><span>758</span><span class="_1P1TY coreSpriteHeartSmall"></span></li><li class="-V_eO"><span>17</span><span class="_1P1TY coreSpriteSpeechBubbleSmall"></span></li></ul>">
+    <img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/a0374add18bef1f3e62ed4cfa4cd49c8/5DA7E1B7/t51.2885-15/sh0.08/e35/c7.0.736.736a/s640x640/64784397_2062957977165387_5650540829266693249_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" class="w-100">
+    </div>
+    <div class="col-4">
+    <img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/a0374add18bef1f3e62ed4cfa4cd49c8/5DA7E1B7/t51.2885-15/sh0.08/e35/c7.0.736.736a/s640x640/64784397_2062957977165387_5650540829266693249_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" class="w-100">
+    </div>
+    <div class="col-4">
+    <img src="https://instagram.fcgk6-1.fna.fbcdn.net/vp/a0374add18bef1f3e62ed4cfa4cd49c8/5DA7E1B7/t51.2885-15/sh0.08/e35/c7.0.736.736a/s640x640/64784397_2062957977165387_5650540829266693249_n.jpg?_nc_ht=instagram.fcgk6-1.fna.fbcdn.net" class="w-100">
     </div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CODING
+                    FUTURE FOR WARD
                 </div>
 
                 <div class="links">
